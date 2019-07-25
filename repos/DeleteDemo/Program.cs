ec7336448fc3756ac3e6bc2b74c2633042ad721f
sav_stone <shiweitun@126.com>
2019-07-25 09:14:15 +0800

    委托demo

---
+++ repos/DeleteDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteDemo
+{
+    class Program
+    {
+        public static string AA { get; set; }
+        private static Testdelegate _testdelegate;
+        public static Testdelegate T
+        {
+            //get; set;
+            get
+            {
+                _testdelegate = (args) =>
+                {
+                    return new TestInfo() { ID = args };
+                };
+                return _testdelegate;
+            }
+            set { }
+        }
+        static void Main(string[] args)
+        {
+            var aa = T("100");
+
+
+            //T += (a) => { return new TestInfo { ID = a }; };
+            //T("1111");
+        }
+    }
+
+    public delegate TestInfo Testdelegate(string id);
+
+    public class TestInfo
+    {
+        public string ID { get; set; }
+    }
+}
